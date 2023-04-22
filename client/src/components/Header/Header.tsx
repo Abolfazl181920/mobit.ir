ee5f689cc557268ab6224dd2c651a5e3a98c11db
Abolfazl <abolfazl.moradi.me@gmail.com>
2023-04-22 17:31:26 +0300

    [ Header ]: create basics of header component!

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,6 +1,24 @@
+import { 
+    SearchIcon, UserIcon, ShoppingCart, SalerworkIcon,
+    ArticlesnewsIcon, AmazingofferIcon, CategoryIcon,
+} from '../../assets/icons'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='h-20 flex w-screen bg-red-700'>
+        <div>
+            <CategoryIcon />
+            <AmazingofferIcon />
+            <ArticlesnewsIcon />
+            <SalerworkIcon />
+            <SearchIcon />
+            <input type="text" placeholder='test' name='test' />
+        </div>
+        <div>
+            <UserIcon />
+            <ShoppingCart />
+        </div>
+    </div>
   )
 }
 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Header from '../components/Header/Header'
 
 const Home = () => {
   return (
-    <div>Home
+    <div>
       <Header />
     </div>
   )
