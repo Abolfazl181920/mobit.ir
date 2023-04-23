0cb201bd61b6d37aa3593b37f7a07fb440761626
Abolfazl <abolfazl.moradi.me@gmail.com>
2023-04-23 20:26:22 +0300

    [ Style ]: bold font and fixed spans in header beside of icons!

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -6,31 +6,31 @@ import {
 const Header = () => {
   return (
     <header className='h-14 flex flex-row-reverse justify-center w-full bg-gradient-to-r from-blue-800 to-transparent to-40% bg-blue-700'>
-        <div className='flex relative left-[14rem] mt-[1.2rem]'>
+        <div className='flex relative font-bold left-[14rem] mt-[1.2rem]'>
 			{/* <SearchIcon /> */}
 			<span className='flex text-[10px] text-white'>
-				<span className='mr-2'>
+				<span className='mr-3 mt-[.15rem]'>
 					همکار و قیمت عمده
 				</span>
 				<SalerworkIcon />
 			</span>
 
 			<span className='flex text-[10px] text-white'>
-				<span className='mr-2'>
+				<span className='mr-3 mt-[.15rem]'>
 					اخبار و مقالات روز
 				</span>
 				<ArticlesnewsIcon />
 			</span>
 
 			<span className='flex text-[10px] text-white'>
-				<span className='mr-2'>
+				<span className='mr-3 mt-[.15rem]'>
 					پیشنهادهای شگفت انگیز
 				</span>
 				<AmazingofferIcon />
 			</span>
 
 			<span className='flex text-[10px] text-white'>
-				<span className='mr-2'>
+				<span className='mr-3 mt-[.15rem]'>
 					دسته بندی
 				</span>
 				<CategoryIcon />
@@ -39,13 +39,13 @@ const Header = () => {
 
 		<div className='flex relative text-[10px] right-[17rem] mt-[1.2rem] text-white'>
 			<span className='flex'>
-				<span className='mr-2'>
+				<span className='mr-3 mt-[.15rem]'>
 					سبد خرید
 				</span>
 				<ShoppingCart />
 			</span>
 			<span className='flex'>
-				<span className='mr-2'>
+				<span className='mr-3 mt-[.15rem]'>
 					ورود/ ثبت نام
 				</span>
 				<UserIcon />
