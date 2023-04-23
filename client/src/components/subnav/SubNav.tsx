2af2ac59c5d1acb4c985c9b2df2910ce872a35cd
Abolfazl <abolfazl.moradi.me@gmail.com>
2023-04-23 18:18:37 +0300

    [ Subnav ]: fixed widths and add pointer!

--- client/src/components/subnav/SubNav.tsx
+++ client/src/components/subnav/SubNav.tsx
@@ -4,42 +4,42 @@ const SubNav = () => {
   return (
     <div className='flex text-center items-center justify-center gap-[5.5rem] mt-52 font-medium text-[.70rem] text-gray-700'>
       <span>
-        <img className='w-14' src={Image.Img6} alt="smartwatch" />
+        <img className='w-14 cursor-pointer' src={Image.Img6} alt="smartwatch" />
         <span>ساعت هوشمند</span>
       </span>
 
       <span>
-        <img className='w-14' src={Image.Img19} alt="headphone" />
+        <img className='w-14 cursor-pointer' src={Image.Img19} alt="headphone" />
         <span>هندزفری</span>
       </span>
 
       <span>
-        <img className='w-14' src={Image.Img17} alt="powerbank" />
+        <img className='w-14 cursor-pointer' src={Image.Img17} alt="powerbank" />
         <span>پاوربانک</span>
       </span>
 
       <span>
-	  	<img className='w-14' src={Image.Tablet} alt="tablet" />
+	  	<img className='w-14 cursor-pointer' src={Image.Tablet} alt="tablet" />
 		    <span>تبلت</span>
       </span>
 
       <span>
-	  	  <img className='w-14' src={Image.Img11} alt="laptop" />
+	  	  <img className='w-14 cursor-pointer' src={Image.Img11} alt="laptop" />
 		    <span>لپتاپ</span>
       </span>
 
       <span>
-	  	  <img className='w-14' src={Image.Img10} alt="computer" />
+	  	  <img className='w-14 cursor-pointer' src={Image.Img10} alt="computer" />
 		    <span>قطعات کامپیوتر</span>
       </span>
 
       <span>
-	  	  <img className='w-16' src={Image.Img18} alt="stuffts" />
+	  	  <img className='w-14 cursor-pointer' src={Image.Img18} alt="stuffts" />
 		    <span>جانبی موبایل</span>
       </span>
 
       <span>
-	  	  <img className='w-16' src={Image.Img7} alt="phone" />
+	  	  <img className='w-14 cursor-pointer' src={Image.Img7} alt="phone" />
 		    <span>گوشی موبایل</span>
       </span>
     </div>
