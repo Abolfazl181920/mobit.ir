4e911ea8a100e9ae372d6f598869e67b50f6343f
Abolfazl <abolfazl.moradi.me@gmail.com>
2023-04-26 15:49:28 +0300

    [ Debug ]: debug a ui issue in header!

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -6,55 +6,55 @@ import SearchInput from './SearchInput'
 import './header.css'
 
 const Header = () => {
-  return (
-    <header className='h-14 flex flex-row-reverse justify-center w-full bg-gradient-to-r from-blue-800 to-transparent to-40% bg-blue-700'>
-        <div className='flex relative font-bold left-[2rem] mt-[1.2rem]'>	
-		<SearchInput />
-			<span className='headeritem'>
-				<span className='iconitem hover-underline-animation'>
-					همکار و قیمت عمده
+	return (
+		<header className='h-14 flex flex-row-reverse justify-center w-full bg-gradient-to-r from-blue-800 to-transparent to-40% bg-blue-700'>
+			<div className='flex relative font-bold left-[2rem] mt-[1.2rem]'>	
+			<SearchInput />
+				<span className='headeritem'>
+					<span className='iconitem hover-underline-animation'>
+						همکار و قیمت عمده
+					</span>
+					<SalerworkIcon />
 				</span>
-				<SalerworkIcon />
-			</span>
 
-			<span className='headeritem'>
-				<span className='iconitem hover-underline-animation'>
-					اخبار و مقالات روز
+				<span className='headeritem'>
+					<span className='iconitem hover-underline-animation'>
+						اخبار و مقالات روز
+					</span>
+					<ArticlesnewsIcon />
 				</span>
-				<ArticlesnewsIcon />
-			</span>
 
-			<span className='headeritem'>
-				<span className='iconitem hover-underline-animation'>
-					پیشنهادهای شگفت انگیز
+				<span className='headeritem'>
+					<span className='iconitem hover-underline-animation'>
+						پیشنهادهای شگفت انگیز
+					</span>
+					<AmazingofferIcon />
 				</span>
-				<AmazingofferIcon />
-			</span>
 
-			<span className='headeritem'>
-				<span className='iconitem hover-underline-animation'>
-					دسته بندی
+				<span className='headeritem'>
+					<span className='iconitem hover-underline-animation'>
+						دسته بندی
+					</span>
+					<CategoryIcon />
 				</span>
-				<CategoryIcon />
-			</span>
-		</div>
+			</div>
 
-		<div className='headeritem relative right-[4rem] mt-[1.2rem]'>
-			<span className='flex'>
-				<span className='iconitem hover-underline-animation'>
-					سبد خرید
+			<div className='headeritem relative right-[4rem] mt-[1.2rem]'>
+				<span className='flex'>
+					<span className='iconitem hover-underline-animation'>
+						سبد خرید
+					</span>
+					<ShoppingCart />
 				</span>
-				<ShoppingCart />
-			</span>
-			<span className='flex'>
-				<span className='iconitem hover-underline-animation'>
-					ورود/ ثبت نام
+				<span className='flex'>
+					<span className='iconitem hover-underline-animation'>
+						ورود/ ثبت نام
+					</span>
+					<UserIcon />
 				</span>
-				<UserIcon />
-			</span>
-		</div>
-    </header>
-  )
+			</div>
+		</header>
+	)
 }
 
 export default Header
